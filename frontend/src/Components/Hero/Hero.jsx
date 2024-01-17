aaa45db58f11b2b87254c85f63484255c1ab72d4
Yogesh Navghane <124075039+yogeshNavghane67@users.noreply.github.com>
2024-01-17 14:28:48 +0530

    Home page

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Hero.css';
 import hand_icon from '../Assets/hand_icon.png';
+import arrow_icon from '../Assets/arrow.png';
+import hero_image from '../Assets/hero_image.png';
 
 const Hero = () => {
   return (
@@ -8,14 +10,20 @@ const Hero = () => {
         <div className="hero-left">
             <h2>NEW ARRIVALS ONLY</h2>
             <div>
-                <div className="hand-hand-icon">
+                <div className="hero-hand-icon">
                     <p>new</p>
                     <img src={hand_icon} alt=''/>
                 </div>
+                <p>collections</p>
+                <p>for everyone</p>
+            </div>
+            <div className="hero-latest-btn">
+                <div>Latest Collection</div>
+                <img src={arrow_icon} alt=''/>
             </div>
         </div>
         <div className="hero-right">
-
+            <img src={hero_image} alt=''/>
         </div>
     </div>
   )
