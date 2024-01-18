2e1a6f94f345b145380af69adfe3edda49a2ebc4
Yogesh Navghane <124075039+yogeshNavghane67@users.noreply.github.com>
2024-01-18 22:32:17 +0530

    cart

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import "./productDisplay.css";
 import star_icon from '../Assets/star_icon.png';
 import star_dull_icon from "../Assets/star_dull_icon.png";
+import { useContext } from "react";
+import { ShopContext } from "../../Context/ShopContext";
 
 const ProductDisplay = (props) => {
     const { product} = props;
+    const {addToCart} = useContext(ShopContext);
   return (
     <div className="productdisplay">
       <div className="productdisplay-left">
@@ -29,8 +32,9 @@ const ProductDisplay = (props) => {
             <p>(112)</p>
         </div>
         <div className="productdiaplay-right-prices">
+        <div className="productdisplay-right-price-old">${product.old_price}</div>
         <div className="productdisplay-right-price-new">${product.new_price}</div>
-            <div className="productdisplay-right-price-old">${product.old_price}</div>
+            
             
         </div>
         <div className="productdisplay-right-description">
@@ -46,7 +50,7 @@ const ProductDisplay = (props) => {
                 <div>XXL</div>
             </div>
         </div>
-        <button>ADD TO CART</button>
+        <button onClick={()=>{addToCart(product.id)}}>ADD TO CART</button>
         <p className="productdisplay-right-category"><span>Category :</span>Women, T-shirt, Crop Top</p>
         <p className="productdisplay-right-category"><span>Tags :</span>Modern, Latest</p>
       </div>
