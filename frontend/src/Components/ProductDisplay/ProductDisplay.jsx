fd014e43c1d3bd3dea43fce421be38642b3f151c
Yogesh Navghane <124075039+yogeshNavghane67@users.noreply.github.com>
2024-01-18 13:40:43 +0530

    product display

---
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./productDisplay.css";
+import star_icon from '../Assets/star_icon.png';
+import star_dull_icon from "../Assets/star_dull_icon.png";
+
+const ProductDisplay = (props) => {
+    const { product} = props;
+  return (
+    <div className="productdisplay">
+      <div className="productdisplay-left">
+        <div className="productdisplay-img-list">
+            <img src={product.image} alt="" />
+            <img src={product.image} alt="" />
+            <img src={product.image} alt="" />
+            <img src={product.image} alt="" />
+        </div>
+        <div className="productdisplay-img">
+            <img className="productdisplay-main-img" src={product.image} alt="" />
+        </div>
+      </div>
+      <div className="productdisplay-right">
+        <h1>{product.name}</h1>
+        <div className="productdisplay-right-stars">
+            <img src={star_icon} alt="" />
+            <img src={star_icon} alt="" />
+            <img src={star_icon} alt="" />
+            <img src={star_icon} alt="" />
+            <img src={star_dull_icon} alt="" />
+            <p>(112)</p>
+        </div>
+        <div className="productdiaplay-right-prices">
+            <div className="productdisplay-right-price-old">${product.old_price}</div>
+            <div className="productdisplay-right-price-new">${product.new_price}</div>
+        </div>
+        <div className="productdisplay-right-description">
+        A T-shirt (also spelled tee shirt, or tee for short) is a style of fabric shirt named after the T shape of its body and sleeves. Traditionally, it has short sleeves and a round neckline, known as a crew neck, which lacks a collar.
+        </div>
+        <div className="productdisplay-right-size">
+            <h1>Select Size</h1>
+            <div className="productdisplay-right-sizes">
+                <div>S</div>
+                <div>M</div>
+                <div>L</div>
+                <div>XL</div>
+                <div>XXL</div>
+            </div>
+        </div>
+        <button>ADD TO CART</button>
+        <p className="productdisplay-right-category"><span>Category :</span>Women, T-shirt, Crop Top</p>
+        <p className="productdisplay-right-category"><span>Tags :</span>Modern, Latest</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDisplay;
