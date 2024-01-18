f2ef2773d1645e5c845a14b06cb6095aca9e89c2
Yogesh Navghane <124075039+yogeshNavghane67@users.noreply.github.com>
2024-01-18 23:48:49 +0530

    cart item count

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 
 
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom'
+import { ShopContext } from '../../Context/ShopContext'
 
 const Navbar = () => {
-    const [menu,setMenu] = useState("Shop")
+    const [menu,setMenu] = useState("Shop");
+    const {getTotalCartItems} = useContext(ShopContext)
   return (
     <div className='navbar'>
 
@@ -25,7 +27,7 @@ const Navbar = () => {
         <div className="nav-login-cart">
             <Link to='/login'><button>Login</button></Link>
             <Link to='/cart'><img src={cart_icon} alt=''/></Link>
-            <div className='nav-cart-count'>0</div>
+            <div className='nav-cart-count'>{getTotalCartItems()}</div>
         </div>
     </div>
   )
--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -39,7 +39,19 @@ const ShopContextProvider = (props) => {
         return totalAmount;
     }
 
-    const contextValue = {getTotalCartAmount,all_product,cartItems,addToCart,removeFromCart};
+    const getTotalCartItems = () => {
+        let totalItem = 0;
+        for (const item in cartItems )
+        {
+            if(cartItems[item]>0)
+            {
+                totalItem += cartItems[item];
+            }
+        }
+        return totalItem;
+    }
+
+    const contextValue = {getTotalCartItems,getTotalCartAmount,all_product,cartItems,addToCart,removeFromCart};
 
 
     return(
