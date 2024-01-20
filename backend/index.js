d93403d0079c8ef302f16b840672feffef5cd583
Yogesh Navghane <124075039+yogeshNavghane67@users.noreply.github.com>
2024-01-20 16:28:00 +0530

    login/ logout functionality

--- backend/index.js
+++ backend/index.js
@@ -179,6 +179,30 @@ app.post('/signup',async(req,res)=>{
     res.json({success:true,token})
 })
 
+// creating endpoint for user login
+app.post('/login',async (req,res)=>{
+    let user = await Users.findOne({email:req.body.email});
+    if(user){
+        const passCompare = req.body.password === user.password;
+        if(passCompare){
+            const data ={
+                user:{
+                    id: user.id
+                }
+            }
+            const token = jwt.sign(data,'secret_ecom');
+            res.json({success:true,token});
+        }
+        else{
+            res.json({success:false,error:"Wrong Password"});
+        }
+    }
+   
+    else{
+        res.json({success:false,error:"Wrong Email Id"})
+    }
+})
+
 app.listen(port,(error)=>{
     if(!error){
         console.log("Server Running on Port" + port);
