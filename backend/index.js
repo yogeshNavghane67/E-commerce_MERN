92d0b0ab5783185ef477143ac3f000fbecb7e5e1
Yogesh Navghane <124075039+yogeshNavghane67@users.noreply.github.com>
2024-01-20 07:31:41 +0530

    registering new user

--- backend/index.js
+++ backend/index.js
@@ -127,6 +127,58 @@ app.get('/allproducts',async (req,res)=>{
     res.send(products);
 })
 
+// Schema creating for user model
+
+const Users = mongoose.model('Users',{
+    name:{
+        type:String,
+    },
+    email:{
+        type: String,
+        unique: true,
+    },
+    password:{
+        type:String,
+    },
+    cartData:{
+        type:Object,
+    },
+    date:{
+        type: Date,
+        default: Date.now,
+    }
+})
+
+
+// Creating Endpoint for registering the user
+app.post('/signup',async(req,res)=>{
+    let check = await Users.findOne({email:req.body.email});
+    if(check){
+        return res.status(400).json({success:false,error:'existing user found with same userid or email address'})
+    }
+    let cart = {};
+    for (let i = 0; i < 300; i++) {
+        cart[i]=0;
+    }
+    const user = new Users({
+        name: req.body.username,
+        email: req.body.email,
+        password:req.body.password,
+        cartData:cart,
+    }) 
+        
+    await user.save();
+
+    const data ={
+        user:{
+            id:user.id
+        }
+    }
+
+    const token = jwt.sign(data,'secret_ecom');
+    res.json({success:true,token})
+})
+
 app.listen(port,(error)=>{
     if(!error){
         console.log("Server Running on Port" + port);
